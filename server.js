01e01b3a1ee830b3398978e9d90c403121d65011
andrea hedström <andrea.hedstrom@outlook.com>
2023-08-31 13:41:17 +0200

    get thoughts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-happy-thoughts-api";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,10 +16,53 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const { Schema } = mongoose
+
+const HappyThoughtSchema = new Schema({
+  message: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 120,
+    trim: true
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
+})
+
+const HappyThoughts = mongoose.model("HappyThought", HappyThoughtSchema)
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(listEndpoints(app));
 });
+
+app.get("/thoughts", async (req, res) => {
+  const happyThoughts = await HappyThoughts.find().sort({ createdAt: 'desc' }).limit(20)
+  try {
+    res.status(200).json({
+      success: true,
+      response: happyThoughts,
+      message: "Some happy thoughts where found!"
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      response: error,
+      message: "Sorry, no happy thoughts here"
+    })
+  }
+})
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body
+})
 
 // Start the server
 app.listen(port, () => {
